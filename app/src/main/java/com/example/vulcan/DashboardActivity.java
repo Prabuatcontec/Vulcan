91c9eaa82ad89032df755f05a4e80fa82c9cf048
Prabu M <mprabu@gocontec.com>
2021-01-08 17:27:27 +0530

    Pie Chart

--- app/src/main/java/com/example/vulcan/DashboardActivity.java
+++ app/src/main/java/com/example/vulcan/DashboardActivity.java
@@ -13,6 +13,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -27,13 +28,25 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.android.material.navigation.NavigationView;
 import com.google.zxing.Result;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    TextView tvR, tvPython, tvCPP, tvJava;
+    PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("Dashboard", "123");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        tvR = findViewById(R.id.tvR);
+        tvPython = findViewById(R.id.tvPython);
+        tvCPP = findViewById(R.id.tvCPP);
+        tvJava = findViewById(R.id.tvJava);
+        pieChart = findViewById(R.id.piechart);
+
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,6 +59,42 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        setData();
+    }
+
+    private void setData()
+    {
+
+        // Set the percentage of language used
+        tvR.setText(Integer.toString(40));
+        tvPython.setText(Integer.toString(30));
+        tvCPP.setText(Integer.toString(5));
+        tvJava.setText(Integer.toString(25));
+
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        Integer.parseInt(tvR.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        Integer.parseInt(tvPython.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        Integer.parseInt(tvCPP.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        Integer.parseInt(tvJava.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+
+        // To animate the pie chart
+        pieChart.startAnimation();
     }
 
     @Override
---
+++ app/src/main/java/com/example/vulcan/MainActivity1.java
@@ -0,0 +1,139 @@
+package com.example.vulcan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.vision.barcode.Barcode;
+import com.example.vulcan.BarcodeReaderActivity;
+import com.example.vulcan.BarcodeReaderFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+
+public class MainActivity1 extends AppCompatActivity implements View.OnClickListener, BarcodeReaderFragment.BarcodeReaderListener {
+    private static final int BARCODE_READER_ACTIVITY_REQUEST = 1208;
+    private TextView mTvResult;
+    private TextView mTvResultHeader;
+    HashMap<Integer, String> serials = new HashMap<Integer, String>();
+    TreeMap<Integer, String> sorted = new TreeMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_main);
+//        findViewById(R.id.btn_fragment).setOnClickListener(this);
+        addBarcodeReaderFragment();
+        mTvResult = findViewById(R.id.tv_result);
+        //mTvResultHeader = findViewById(R.id.tv_result_head);
+    }
+
+    private void addBarcodeReaderFragment() {
+        BarcodeReaderFragment readerFragment = BarcodeReaderFragment.newInstance(true, false, View.VISIBLE);
+        readerFragment.setListener(this);
+        FragmentManager supportFragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fm_container, readerFragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.btn_fragment:
+//                addBarcodeReaderFragment();
+//                break;
+//            case R.id.btn_activity:
+//                FragmentManager supportFragmentManager = getFragmentManager();
+//                FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+//                Fragment fragmentById = supportFragmentManager.findFragmentById(R.id.fm_container);
+//                if (fragmentById != null) {
+//                    fragmentTransaction.remove(fragmentById);
+//                }
+//                fragmentTransaction.commitAllowingStateLoss();
+//                launchBarCodeActivity();
+//                break;
+//        }
+    }
+
+
+    private void launchBarCodeActivity() {
+        Intent launchIntent = BarcodeReaderActivity.getLaunchIntent(this, true, false);
+        startActivityForResult(launchIntent, BARCODE_READER_ACTIVITY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "error in  scanning", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (requestCode == BARCODE_READER_ACTIVITY_REQUEST && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeReaderActivity.KEY_CAPTURED_BARCODE);
+            Toast.makeText(this, barcode.rawValue, Toast.LENGTH_SHORT).show();
+            //mTvResultHeader.setText("On Activity Result");
+            mTvResult.setText(barcode.rawValue);
+
+            Log.e("BARCODE-SCANNER", "Retrieving barcode data");
+            String dbarcodes = data.getStringExtra(BarcodeReaderActivity.KEY_CAPTURED_RAW_BARCODES);
+            mTvResult.setText(dbarcodes);
+        }
+
+
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+
+        if(serials.containsValue(barcode.rawValue.replaceAll("[-+^]*", "")) == false)
+        {
+            serials.put(barcode.cornerPoints[0].x, barcode.rawValue.replaceAll("[-+^]*", "")
+            );
+            Log.d("Barcode", serials.toString().replace(",",","));
+        }
+        sorted.putAll(serials);
+        //Toast.makeText(this, barcode.rawValue, Toast.LENGTH_SHORT).show();
+        //mTvResultHeader.setText("Barcode value from fragment");
+        mTvResult.setText(serials.toString().replace(",",","));
+    }
+
+
+
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(this, "Camera permission denied!", Toast.LENGTH_LONG).show();
+    }
+}
